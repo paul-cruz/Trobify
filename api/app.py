70a6f31c807b2c57f135f59ca202e93a27265421
paul-cruz <poljet12@gmail.com>
2021-04-14 14:55:05 -0500

    API init commit

---
+++ api/app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return jsonify({'status': 'api working'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))

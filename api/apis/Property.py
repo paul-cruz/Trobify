09d4cd8beba5b63a88c4e604a51a248344c6b0b9
paul-cruz <poljet12@gmail.com>
2021-05-04 12:08:37 -0500

    Completed: Properties basic CRUD

--- api/apis/Property.py
+++ api/apis/Property.py
@@ -1,65 +1,127 @@
 import os
 import pymongo
 from flask import jsonify
+from bson.objectid import ObjectId
 from bson.json_util import dumps, loads
+from pymongo.collection import ReturnDocument
 from flask_restplus import Namespace, Resource, fields
 
 myclient = pymongo.MongoClient(os.getenv("BD_CONN"))
 db = myclient[os.getenv("DB_NAME")]
-mycol = db["properties"]
+propCol = db["properties"]
 api = Namespace('properties', description='properties related operations')
 
-apoiment = {
+apoiment = api.model('Apoiment', {
     "ref": fields.Integer(),
     "date": fields.Date()
-}
+})
+
 property = api.model('Properties', {
     "tittle": fields.String(),
     "price": fields.Integer(),
-    "features": {
-        "land": fields.Decimal(),
-        "built": fields.Decimal(),
-        "rooms": fields.Integer(),
-        "bathrooms": fields.Integer(),
-        "kitchens": fields.Integer(),
-        "garage": fields.Integer()
-    },
-    "address": {
-        "line1": fields.String(),
-        "state": fields.String(),
-        "zip": fields.String(),
-        "city": fields.String()
-    },
-    "location": {
-        "lat": fields.Decimal(),
-        "long": fields.Decimal()
-    },
+    "features": fields.Raw(example={
+        "land": 0.0,
+        "built": 0.0,
+        "rooms": 0,
+        "bathrooms": 0,
+        "kitchens": 0,
+        "garage": 0
+    }),
+    "address": fields.Raw(example={
+        "line1": "",
+        "state": "",
+        "zip": "",
+        "city": ""
+    }),
+    "location": fields.Raw(example={
+        "lat": 0.0,
+        "long": 0.0
+    }),
     "description": fields.String(),
     "type": fields.String(),
     "age": fields.Integer(),
     "floors": fields.Integer(),
-    "contact": {
-        "name": fields.String(),
-        "phones": fields.List(fields.String),
-        "email": fields.String()
-    },
+    "contact": fields.Raw(example={
+        "name": "",
+        "phones": [""],
+        "email": ""
+    }),
     "apoiments": fields.List(fields.Nested(apoiment))
 })
 
 
 @api.route('/')
+@api.response(404, 'Property not inserted')
+@api.response(500, 'Server Error')
 class Properties(Resource):
     @api.doc('list_properties')
     def get(self):
-        return dumps(list(mycol.find()))
+        return list(propCol.find())
+
+    @api.doc('post_property')
+    @api.expect(property)
+    def post(self):
+        try:
+            result_id = propCol.insert_one(api.payload).inserted_id
+            if result_id:
+                return {'msg': 'Inserted'}, 201
+            raise ValueError('Property not found')
+        except ValueError as ve:
+            print('Property exception', ve)
+            api.abort(404)
+        except Exception as e:
+            print('Server Error', e)
+            api.abort(500)
 
 
-'''@api.route('/<id>')
+@api.route('/<id>')
 @api.param('id', 'The property identifier')
-@api.response(404, 'Cat not found')
+@api.response(404, 'Property not found')
+@api.response(500, 'Server Error')
 class Property(Resource):
-    @api.doc('get_cat')
+    @api.doc('get_property')
     def get(self, id):
-        return {"hola": 1}
-        api.abort(404)
-'''
+        try:
+            result = propCol.find_one({'_id': ObjectId(id)})
+            if result:
+                return dumps(result)
+            raise ValueError('Property not found')
+        except ValueError as ve:
+            print('Property exception', ve)
+            api.abort(404)
+        except Exception as e:
+            print('Server Error', e)
+            api.abort(500)
+
+    @api.doc('get_property')
+    @api.expect(property)
+    def put(self, id):
+        try:
+            doc = api.payload
+            result = propCol.find_one_and_update(
+                {'_id': ObjectId(id)},
+                {'$set': doc},
+                return_document=ReturnDocument.AFTER)
+            if result:
+                return {'msg': 'Updated'}, 200
+            raise ValueError('Property not found')
+        except ValueError as ve:
+            print('Property exception', ve)
+            api.abort(404)
+        except Exception as e:
+            print('Server Error', e)
+            api.abort(500)
+
+    @api.doc('get_property')
+    def delete(self, id):
+        try:
+            result = propCol.find_one_and_delete({'_id': ObjectId(id)})
+            if result:
+                return {'msg': 'Deleted'}, 200
+            raise ValueError('Property not found')
+        except ValueError as ve:
+            print('Property exception', ve)
+            api.abort(404)
+        except Exception as e:
+            print('Server Error', e)
+            api.abort(500)
--- api/app.py
+++ api/app.py
@@ -1,24 +1,30 @@
 import os
 import json
 import datetime
+import decimal
 from apis import api
 from flask import Flask, jsonify
 from bson.objectid import ObjectId
 
+
 class JSONEncoder(json.JSONEncoder):
-    ''' extend json-encoder class'''
-    def default(self, o):
+    def default(self, o):  # pylint: disable=E0202
+        if isinstance(o, decimal.Decimal):
+            return float(o)
         if isinstance(o, ObjectId):
             return str(o)
-        if isinstance(o, datetime.datetime):
-            return str(o)
+        if isinstance(o, (datetime.datetime, datetime.date)):
+            return o.isoformat()
         return json.JSONEncoder.default(self, o)
-        
+
+
 app = Flask(__name__)
 api.init_app(app)
 
 # use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
-app.json_encoder = JSONEncoder
+#app.json_encoder = JSONEncoder
+app.config["RESTPLUS_JSON"] = {"cls": JSONEncoder}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT', 50000),

6756732e1dea364dd9f9222593aa370ecfcd67f4
EduardoMtz1 <hbkeduardo10@hotmail.com>
2021-05-19 18:21:47 -0500

    Adder 'user' namespace

--- api/apis/__init__.py
+++ api/apis/__init__.py
@@ -1,10 +1,13 @@
 from flask_restplus import Api
 from .Property import api as nsProperties
+from .User import api as nsUsers
 
 api = Api(
     title='Trobify API',
     version='1.0',
-    description='this is the trobify API'
+    description='this is the trobify API',
+    prefix='/api'
 )
 
 api.add_namespace(nsProperties, path='/property')
+api.add_namespace(nsUsers, path='/user')

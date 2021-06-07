397ed3a16c9a6785c59ff0b63efc650faf36815e
Mariani3 <matt_3199@hotmail.com>
2021-06-06 23:34:51 -0500

    Vista perfiles y front datos visita

---
+++ web/src/Components/ProfileView/Basic/ModifyProfile.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+        },
+    },
+    button: {
+        margin: theme.spacing(1),
+    }
+}))
+
+function ModifyProfile() {
+
+    const classes = useStyles()
+    const [inputFields, setInputFields] = useState([
+        { firstName: '', lastName: '', email: '', phoneNumber: '' },
+    ]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("InputFields", inputFields);
+    };
+
+    const handleChangeInput = (index, event) => {
+        const values = [...inputFields];
+        values[index][event.target.name] = event.target.value;
+        setInputFields(values);
+    };
+
+    return (
+        <Container className="container text-center">
+            <div className="card mt-4 align-items-center text-center">
+                <div className="card-header">
+                    <h3>Modificar Perfil</h3>
+                </div>
+                <div className="card-body">
+                    <form className={classes.root} onSubmit={handleSubmit}>
+                        {inputFields.map((inputField, index) => (
+                            <div key={index} className="justify-content-center">
+                                <div className="form-group input-group mb-3">
+                                    <TextField
+                                        id="filled-full-width"
+                                        name="firstName"
+                                        label="Nombre"
+                                        fullWidth
+                                        variant="filled"
+                                        value={inputField.firstName}
+                                        onChange={event => handleChangeInput(index, event)}
+                                    />
+                                </div>
+                                <div className="form-group input-group mb-3">
+                                    <TextField
+                                        id="filled-full-width"
+                                        name="lastName"
+                                        label="Apellido"
+                                        fullWidth
+                                        variant="filled"
+                                        value={inputField.lastName}
+                                        onChange={event => handleChangeInput(index, event)}
+                                    />
+                                </div>
+                                <div className="form-group input-group mb-3">
+                                    <TextField
+                                        name="email"
+                                        label="E-mail"
+                                        variant="filled"
+                                        value={inputField.email}
+                                        onChange={event => handleChangeInput(index, event)}
+                                    />
+                                    <TextField
+                                        name="phoneNumber"
+                                        label="Teléfono"
+                                        variant="filled"
+                                        value={inputField.phoneNumber}
+                                        onChange={event => handleChangeInput(index, event)}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            onClick={handleSubmit}
+                        >Guardar
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </Container >
+    )
+}
+
+export default ModifyProfile

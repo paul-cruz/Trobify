9668534d4de2abdbc740778190890d0171854f15
EduardoMtz1 <hbkeduardo10@hotmail.com>
2021-06-10 13:05:17 -0500

    New property view created

---
+++ web/src/Views/NewProperty.js
@@ -0,0 +1,9 @@
+import FormNewProperty from '../Components/FormNewProperty';
+
+export default function NewProperty(){
+    return(
+        <div>
+            <FormNewProperty/>
+        </div>
+    );
+}

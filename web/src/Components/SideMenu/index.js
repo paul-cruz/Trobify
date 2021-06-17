a569bdab39e2df59ca26b5c9f0fec28b16ff935a
paul-cruz <poljet12@gmail.com>
2021-06-17 00:45:42 -0500

    Added: i18n

--- web/src/Components/SideMenu/index.js
+++ web/src/Components/SideMenu/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../Styles/custom.css';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 import ApartmentIcon from '@material-ui/icons/Apartment';
 
 export default function SideMenu() {
@@ -9,14 +10,66 @@ export default function SideMenu() {
         <div className="sideMenu bg-dark text-light link-light" id="sideMenu">
             <h4>Trobify <ApartmentIcon /></h4>
             <ul className="side-ul">
-                <li className="side-li"><button className="btn btn-dark"><Link className="linkMenu" to="/app/properties">Buscar</Link></button></li>
-                <li className="side-li"><button className="btn btn-dark"><Link className="linkMenu" to="/app/grid">Notificaciones</Link></button></li>
-                <li className="side-li"><button className="btn btn-dark">Alertas</button></li>
-                <li className="side-li"><button className="btn btn-dark"><Link className="linkMenu" to="/app/profile">Perfil</Link></button></li>
-                <li className="side-li"><button className="btn btn-dark"><Link className="linkMenu" to="/newproperty">Nuevo</Link></button></li>
-                <li className="side-li"><button className="btn btn-dark">3D</button></li>
-                <li className="side-li"><button className="btn btn-dark">Agenda</button></li>
-                <li className="side-li"><button className="btn btn-dark"><Link className="linkMenu" to="/app/report">Reportar error</Link></button></li>
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <Link className="linkMenu" to="/app/properties">
+                            <FormattedMessage
+                                id="app.menu.search"
+                                defaultMessage="Search"
+                            />
+                        </Link>
+                    </button>
+                </li>
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <Link className="linkMenu" to="/app/grid">
+                            <FormattedMessage
+                                id="app.menu.notif"
+                                defaultMessage="Notifications"
+                            />
+                        </Link>
+                    </button>
+                </li>
+                {/*<li className="side-li"><button className="btn btn-dark">Alertas</button></li>*/}
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <Link className="linkMenu" to="/app/profile">
+                            <FormattedMessage
+                                id="app.menu.profile"
+                                defaultMessage="Profile"
+                            />
+                        </Link>
+                    </button>
+                </li>
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <Link className="linkMenu" to="/newproperty">
+                            <FormattedMessage
+                                id="app.menu.new"
+                                defaultMessage="New"
+                            />
+                        </Link>
+                    </button>
+                </li>
+                {/*<li className="side-li"><button className="btn btn-dark">3D</button></li>*/}
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <FormattedMessage
+                            id="app.menu.agenda"
+                            defaultMessage="Agenda"
+                        />
+                    </button>
+                </li>
+                <li className="side-li">
+                    <button className="btn btn-dark">
+                        <Link className="linkMenu" to="/app/report">
+                            <FormattedMessage
+                                id="app.menu.report"
+                                defaultMessage="Report Error"
+                            />
+                        </Link>
+                    </button>
+                </li>
             </ul>
         </div>
     );
